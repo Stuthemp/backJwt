eeeabeddfa7a5278b2c987349d65bd0fd7fd8375
StuThEmp <semenshulga1@gmail.com>
2021-12-21 17:45:07 +0300

    saving

---
+++ backendJWTpgs/src/main/java/com/example/backendJWTpgs/BackendJwTpgsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendJWTpgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJwTpgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJwTpgsApplication.class, args);
+	}
+
+}
---
+++ backendJWTpgs/src/test/java/com/example/backendJWTpgs/BackendJwTpgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendJWTpgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJwTpgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
